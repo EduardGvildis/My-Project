2bc86bbf82d497f2e5ca78eb903bdc2f3be3f2c3
edikg <edikgvildis@gmail.com>
2025-07-22 09:32:51 +0300

    initial commit

---
+++ NewMain/src/main/java/org/example/hvildismain/HvildisMain.java
@@ -0,0 +1,13 @@
+package org.example.hvildismain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvildisMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvildisMain.class, args);
+    }
+
+}
---
+++ NewMain/src/test/java/org/example/hvildismain/HvildisMainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hvildismain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvildisMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
